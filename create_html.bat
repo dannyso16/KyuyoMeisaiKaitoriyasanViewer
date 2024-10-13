f9636d00aa5d59b2a93a38324dd3f3a33ac0110c
odanny <osatoshi.11@gmail.com>
2024-10-13 20:17:36 +0900

    add:

--- create_html.bat
+++ create_html.bat
@@ -1,1 +1,24 @@
 python .\create_html.py
+git status
+
+set /p yn_check="Git commit? (y/n):  "
+IF %yn_check:Y=Y%==Y (
+  for /f "tokens=2 delims==" %%i in ('wmic os get localdatetime /value ^| find "="') do set datetime=%%i
+  set commit_date=%datetime:~0,8%
+  git add --a
+  git commit -m "add: %commit_date%"
+  
+  set /p yn_check="Git pushH (y/n):  "
+  IF %yn_check:Y=Y%==Y (
+    --git push
+    pause
+  )
+  ELSE (
+    GOTO DOIT
+  )
+) ELSE (
+  GOTO DOIT
+) 
+
+:DOIT
+start cmd /k echo push‚Æ‚©‚Í©•ª‚Å‚â‚Á‚Ä‚Ë
